a3b1e681040ae5a67487d39706d33bbb71efc0e2
Mehmet Celik <mehmet@celik.be>
2025-01-10 23:34:49 +0000

    adding store reference

--- src/app/libis/dot/dot.component.ts
+++ src/app/libis/dot/dot.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject} from '@angular/core';
+import { createFeatureSelector, Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+
+type SearchState = {searchResultsMetaData : {info: {total: number}}};
+export const selectSearchState = createFeatureSelector<SearchState>('Search');
 
 @Component({
   selector: 'custom-dot',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dot.component.html',
   styleUrl: './dot.component.scss'
 })
 export class DotComponent {
+  searchState$: Observable<SearchState> | undefined;
+  searchTotal$!: Observable<String> | undefined;
 
+  public store = inject(Store);
+  
+  ngOnInit(): void {
+    this.searchState$ = this.store.select(selectSearchState);
+    this.searchTotal$ = this.searchState$.pipe(map(state => {
+      console.log(state);  
+      if (state.searchResultsMetaData) {
+        return state.searchResultsMetaData.info.total.toString();
+      } else {
+        return "0";
+      }}));    
+    
+    (globalThis as any).searchStore = this.searchState$;
+    (globalThis as any).searchTotal = this.searchTotal$;
+  }
+ 
 }
